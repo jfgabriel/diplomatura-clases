a7ea6cdc7cc9adaa63f7d196f4bf7701fb89eb24
jfgabriel <gjuan3000@gmail.com>
2020-05-10 14:27:47 -0300

    update eventloop

--- src/001-eventloop.js
+++ src/001-eventloop.js
@@ -1,10 +1,10 @@
-// Muestra 'Hello' en la consola
-console.log("Hello");
+// Muestra 'Hola' en la consola
+console.log("Hola");
 
 // Espera 5 segundos y muestra 'Done'
-setTimeout(function dormir() {
-  console.log("Done");
-}, 5000);
+setTimeout(function esperar() {
+  console.log("estás");
+}, 2000);
 
-// Muestra 'World' en la consola
-console.log("World");
+// Muestra 'como' en la consola
+console.log("como");
